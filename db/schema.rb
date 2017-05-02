a3df4a33fde6d842729248cf4eea92611508d571
Brian Wilson <bountonw@gmail.com>
2017-05-02 23:13:01 +0000

    Add enrollment feature for courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502162414) do
+ActiveRecord::Schema.define(version: 20170502223357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20170502162414) do
     t.integer "user_id"
     t.string  "image"
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id", using: :btree
+    t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id", using: :btree
   end
 
   create_table "images", force: :cascade do |t|
